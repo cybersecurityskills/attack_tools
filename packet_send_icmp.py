29e69c265aa952722f1717cdf7a2599c727743b9
cybersecurityskills <139646528+cybersecurityskills@users.noreply.github.com>
2023-08-09 11:30:40 +0200

    Create packet_send_icmp.py

---
+++ packet_send_icmp.py
@@ -0,0 +1,10 @@
+from scapy.all import *
+import sys
+
+if len(sys.argv) < 2:
+  print("Usage: packet_send_ttl.py SRC_IP DST_IP ICMP_TYPE\n")  
+  sys.exit()
+
+p1 = IP(src=sys.argv[1],dst=sys.argv[2])/ICMP(type=int(sys.argv[3]))
+print("Sending",p1)
+send(p1) 
